23704cf9364b2666183ad78200ddb7d3dfd7d219
cbharp <cbharp11@gmail.com>
2023-09-09 15:27:28 -0400

    second commit of functions calling script

--- banking_dashboard/functions_calling_script.py
+++ banking_dashboard/functions_calling_script.py
@@ -1,7 +1,10 @@
 from helper_fcns import *
 
 # ingest census tract data 
-census_data_ingester()
+census_data = census_data_ingester()
 
 # ingest ffiec census data 
-ffiec_flat_file_extractor()
+ffiec_data = ffiec_flat_file_extractor()
+
+#ingest files from HDMA website
+hdma_dict = hdma_helper_fcn()
